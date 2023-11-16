701165d5061a19a96997a7cdfc04b4800951024a
Shyam-Sundar-7 <mailshyamsundar.2022@gmail.com>
2023-11-17 00:02:07 +0530

    adding most efficient method

--- 005 evenly_divisible.py
+++ 005 evenly_divisible.py
@@ -32,3 +32,17 @@ print("time taken",end-start)
 #         break
 
 # print(num)
+"""
+Most efficient method
+import math
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+result = 1
+
+for i in range(1, 21):
+    result = lcm(result, i)
+
+print(result)
+"""
